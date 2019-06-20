ee719a8db9fccc68f3560c3060faab10816ea3d6
Aman2 <aman-goyal>
2019-06-21 03:08:26 +0530

    Exam in BerSU Slight Modification

--- Exam in BerSU.py
+++ Exam in BerSU.py
@@ -1,11 +1,8 @@
 n, m = map(int, input().split())
 s = list(map(int, input().split()))
-# [80 40 40 40 60]
 print('0', end=' ')
 for i in range(1,n):
     student_fail = 0
-
-    # print(student_fail)
     new_lst = s[:i+1]
     while sum(new_lst) > m:
         new_lst.remove(max(new_lst[:-1]))
