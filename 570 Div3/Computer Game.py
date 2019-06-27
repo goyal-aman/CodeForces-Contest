352590d8ae1beed3e2ff78906d9600f571258bf1
Aman2 <aman-goyal>
2019-06-27 22:55:45 +0530

    Computer Game Documentation added (Hinglish) Completed

--- 570 Div3/Computer Game.py
+++ 570 Div3/Computer Game.py
@@ -1,3 +1,19 @@
+"""
+1.phele check kro ki given charge k, n*a se jyada h ya nh
+        if yes, then print n, and move on to next query
+        if no, then goto step 2
+2. calculate charge needed by k - n*a,
+        ye btaega ki kitne charge ki kaami pad rhi h jab hum sare turn a ki hisab se chl rhe h
+
+    charge_needed bta rha h ki kitna charge kam h, b ham bari bari har turn ke lie a move ko b move se replace krna chate h,
+    jisse battry ka charge  a-b se badega,
+
+3. pta kro ki kitni bar a wale move ko b se replace krna padega, - (turn)
+
+4. agr turn needed n se bada h to print -1, vrna print turn-n
+
+"""
+
 for _ in range(int(input())):
     k, n, a, b = map(int, input().split())
     if k>n*a:
