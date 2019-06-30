bd2484c685b18369a43c0633d56c7e83e0dd25ce
Aman2 <aman-goyal>
2019-06-30 16:54:38 +0530

    Vus the Cossack and Numbers Better Solution | Less space Completed

--- 571 Div2/Vus the Cossack and Numbers.py
+++ 571 Div2/Vus the Cossack and Numbers.py
@@ -2,30 +2,58 @@ import sys
 from math import *
 n = int(input())
 org = [0]*n
-f = [0]*n
-c = [0]*n
 sum_f = 0
 for i in range(n):
     num = float(sys.stdin.readline().rstrip())
     org[i] = num
-    f[i] = floor(num)
-    sum_f += f[i]
-    c[i] = ceil(num)
-
+    sum_f += floor(num)
 i = 0
 while sum_f<0 and i<n:
-    if f[i]<c[i]:
+
+    if floor(org[i])<ceil(org[i]):
         sum_f+=1
-        f[i]=c[i]
+        org[i]=ceil(org[i])
+    else:
+        org[i] = floor(org[i])
     i+=1
-#edge case: In any case we cannot get sum_f =0
 if i>n:
     print(-1)
     exit()
 
     i+=1
-for i in f:
-    print(i)
+for i in org:
+    print(floor(i))
+
+
+
+# import sys
+# from math import *
+# n = int(input())
+# org = [0]*n
+# f = [0]*n
+# c = [0]*n
+# sum_f = 0
+# for i in range(n):
+#     num = float(sys.stdin.readline().rstrip())
+#     org[i] = num
+#     f[i] = floor(num)
+#     sum_f += f[i]
+#     c[i] = ceil(num)
+#
+# i = 0
+# while sum_f<0 and i<n:
+#     if f[i]<c[i]:
+#         sum_f+=1
+#         f[i]=c[i]
+#     i+=1
+# #edge case: In any case we cannot get sum_f =0
+# if i>n:
+#     print(-1)
+#     exit()
+#
+#     i+=1
+# for i in f:
+#     print(i)
 
 
 
